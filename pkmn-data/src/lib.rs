d5032beb2385fa36e9d7427bbccf03d8c7e6c491
Nik Deapen <nik@deapen.com>
2026-06-24 12:04:23 -0700

    [core] Added support for loading pokemon data.

--- pkmn-data/src/lib.rs
+++ pkmn-data/src/lib.rs
@@ -1,1 +1,3 @@
+pub use pokemon::*;
 
+mod pokemon;
---
+++ pkmn-data/src/pokemon.rs
@@ -0,0 +1,61 @@
+use pkmn_core::validate::validate_unique_names;
+use pkmn_schema::pokemon::Pokemon;
+use std::collections::HashMap;
+use std::sync::LazyLock;
+
+/// Gets the pokemon. (indexed by `dex - 1`)
+#[must_use]
+pub fn pokemon() -> &'static [Pokemon] {
+    static LOCK: LazyLock<Vec<Pokemon>> = LazyLock::new(|| {
+        let pokemon: &str = include_str!("../../data/pokemon.yml");
+        let pokemon: Vec<Pokemon> = serde_yaml::from_str(pokemon).unwrap_or_else(|e| panic!("{e}"));
+        validate(&pokemon).unwrap_or_else(|e| panic!("{e}"));
+        pokemon
+    });
+    &LOCK
+}
+
+/// Gets the optional pokemon with the national `dex` number.
+#[must_use]
+pub fn pokemon_by_dex(dex: u32) -> Option<&'static Pokemon> {
+    pokemon().get((dex as usize).checked_sub(1)?)
+}
+
+/// The pokemon keyed by `name.id`.
+#[must_use]
+pub fn pokemon_by_id() -> &'static HashMap<&'static str, &'static Pokemon> {
+    static LOCK: LazyLock<HashMap<&'static str, &'static Pokemon>> =
+        LazyLock::new(|| pokemon().iter().map(|p| (p.name().id(), p)).collect());
+    &LOCK
+}
+
+fn validate(pokemon: &[Pokemon]) -> Result<(), String> {
+    for (i, p) in pokemon.iter().enumerate() {
+        let expected_dex: u32 = (i + 1) as u32;
+        if p.dex() != expected_dex {
+            return Err(format!(
+                "pokemon[{i}] has dex: {} (expected {expected_dex})",
+                p.dex()
+            ));
+        }
+    }
+    validate_unique_names(pokemon.iter().map(Pokemon::name))?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn pokemon() {
+        let all: &[Pokemon] = super::pokemon();
+        assert_eq!(all.len(), 1025);
+    }
+
+    #[test]
+    fn pokemon_by_id() {
+        let map: &HashMap<&str, &Pokemon> = super::pokemon_by_id();
+        assert_eq!(map.len(), 1025);
+    }
+}
